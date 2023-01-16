42e6c00303eb0fb5fd82bf89fbded35285b47183
victorltd <victor.augusto@discente.univasf.edu.br>
2023-01-16 12:48:25 -0300

    usability test

--- Firebase/public/script.js
+++ Firebase/public/script.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
      // Your code to run since DOM is loaded and ready
     });
-
+//ss
 
 
     var myLink = document.querySelector('a[href="#"]');
